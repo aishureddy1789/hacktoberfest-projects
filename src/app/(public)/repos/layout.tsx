a0e043a369d4636a9c3ff90a4b4b2738ddbb85a3
aishureddy1789 <aishureddy1789@gmail.com>
2025-10-28 23:06:04 +0530

    fix:added a suspense

--- src/app/(public)/repos/layout.tsx
+++ src/app/(public)/repos/layout.tsx
@@ -1,8 +1,9 @@
 // app/dashboard/layout.tsx
 import React from 'react';
+import { Suspense } from 'react'
 import Sidebar from '../_components/sidebar';
 import { Header } from '../_components/header';
-
+import { Loading } from "./loading"
 export default function DashboardLayout({
   children
 }: {
@@ -11,7 +12,9 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen">
       <aside className="hidden lg:fixed lg:top-[86px] lg:w-[350px] min-h-[calc(100vh-64px)] lg:flex flex-col bg-muted border-r p-4">
-        <Sidebar />
+        <Suspense fallback={<Loading />}>
+          <Sidebar />
+        </Suspense>
       </aside>
       <main className="flex-1 p-6 overflow-y-auto lg:ml-[350px]">
         <Header />
